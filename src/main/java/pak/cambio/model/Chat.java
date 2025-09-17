9643687e85ac0fe56f4ec2ca29b3ddfc3e14b7c2
Tyler Pak <tyler.j.pak.mil@army.mil>
2025-09-17 10:51:21 -0500

    worked on chat model, and websocket controller

--- src/main/java/pak/cambio/model/Chat.java
+++ src/main/java/pak/cambio/model/Chat.java
@@ -1,4 +1,58 @@
 package pak.cambio.model;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="chat")
 public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="game_id", nullable = false)
+    private Game game;
+    private String message;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+
+    public Chat() {
+
+    }
+
+    public Chat(String message, Game game, User user) {
+        this.message = message;
+        this.game = game;
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 }

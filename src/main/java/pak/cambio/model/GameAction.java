622d8b2f6043f7cb022f291b64de773ee4610004
Tyler Pak <tyler.j.pak.mil@army.mil>
2025-09-29 11:13:55 -0500

    adjusted backend to use payload for swap card functionality

--- src/main/java/pak/cambio/model/GameAction.java
+++ src/main/java/pak/cambio/model/GameAction.java
@@ -1,20 +1,26 @@
 package pak.cambio.model;
 
+import java.util.Map;
+
 public class GameAction {
     private Long userId;
     private String username;
     private ActionType type;
-    private Integer handIndex;
-    private Card card;
+    private Map<String, Object> payload;
 
 
-    public GameAction(Long userId, String username, ActionType type, Integer handIndex, Card card) {
+    public GameAction(Long userId, String username, ActionType type, Map<String, Object> payload) {
         this.userId = userId;
         this.username = username;
         this.type = type;
-        this.handIndex = handIndex;
-        this.card = card;
+        this.payload = payload;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+
 
     public Long getUserId() {
         return userId;
@@ -24,11 +30,20 @@ public class GameAction {
         return type;
     }
 
-    public Integer getHandIndex() {
-        return handIndex;
+    public Integer getInt(String key) {
+        return payload.containsKey(key) ? (Integer) payload.get(key) : null;
     }
 
-    public Card getCard() {
-        return card;
+    public String getString(String key) {
+        return payload.containsKey(key) ? (String) payload.get(key) : null;
     }
+
+    public Long getLong(String key) {
+        return payload.containsKey(key) ? (Long) payload.get(key) : null;
+    }
+
+
+    public Map<String, Object> getPayload() { return payload; }
+    public void setPayload(Map<String, Object> payload) { this.payload = payload; }
+
 }

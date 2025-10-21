401e005d9f49ebf1b436b77fa131546491e763a5
Wyson Kong <wyson.kong.mil@aswf.army.mil>
2025-10-21 10:04:58 -0500

    added tailwind and shadcn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Layout from "./Layout.tsx";
+import Home from "@/pages/Home.tsx";
+import Login from "@/pages/Login.tsx";
+import Signup from "@/pages/Signup.tsx";
+import {AuthProvider} from "@/components/AuthProvider.tsx";
+
+function App() {
+
+  return (
+      <AuthProvider>
+          <Router>
+              <Layout>
+                  <Routes>
+                      <Route path={"/"} element={<Home/>}/>
+                      <Route path={"/Login"} element={<Login/>}/>
+                      <Route path={"/Signup"} element={<Signup/>}/>
+                  </Routes>
+              </Layout>
+          </Router>
+      </AuthProvider>
+  )
+}
+
+export default App

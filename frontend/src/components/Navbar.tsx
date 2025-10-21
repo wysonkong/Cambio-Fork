66892fefe5956485dfc5f2a69d7f4b8d9ddd19ef
Wyson Kong <wyson.kong.mil@aswf.army.mil>
2025-10-21 10:32:10 -0500

    added login logic for navbar and logout

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -66,7 +66,7 @@ const Navbar = () => {
                                 <NavigationMenuLink asChild>
                                     <Link
                                         to="#"
-                                        //onClick={handleLogout}
+                                        onClick={handleLogout}
                                         className={"px-3 py-2 rounded-md hover:bg-secondary hover:text-secondary-foreground transition-colors"}
                                     >
                                         Log Out

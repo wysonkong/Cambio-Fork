85cb3d42bc1cf1c2aed6d452a5a0b571eb6bd57f
Wyson Kong <wyson.kong.mil@aswf.army.mil>
2025-10-22 11:49:30 -0500

    changed to fetch backend db

--- frontend/src/pages/Standings.tsx
+++ frontend/src/pages/Standings.tsx
@@ -8,14 +8,13 @@ export default function Standings() {
 
     useEffect(() => {
         async function fetchData() {
-            const result = [
-                {id: "1", username: "Test", wins: 4, losses: 1},
-                {id: "2", username: "Admin", wins: 2, losses: 1},
-                {id: "3", username: "Yes", wins: 2, losses: 1},
-                {id: "4", username: "OMG", wins: 3, losses: 2},
-                {id: "5", username: "Weird", wins: 5, losses: 1},
-            ];
-            setData(result);
+            try {
+                const response = await fetch("http://localhost:8080/api/standings");
+                const items = await response.json();
+                setData(items);
+            } catch (err) {
+                console.error("Error fetching items:", err);
+            }
         }
 
         fetchData();

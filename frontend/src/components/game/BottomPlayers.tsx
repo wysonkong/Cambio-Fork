8b1e2f6b8dc655fe409d888cdcaadcd5c8460f57
Wyson Kong <wyson.kong.mil@aswf.army.mil>
2025-11-03 11:04:52 -0600

    avatar images and pending card conditionals

--- frontend/src/components/game/BottomPlayers.tsx
+++ frontend/src/components/game/BottomPlayers.tsx
@@ -1,6 +1,7 @@
 import CardHand from "@/components/game/cards/CardHand.tsx";
 import type {Player, CardType} from "@/components/Interfaces.tsx";
 import {useEffect, useState} from "react";
+import {Card} from "@/components/game/cards/Card.tsx";
 
 interface BottomPlayersProp {
     player: Player,
@@ -8,6 +9,7 @@ interface BottomPlayersProp {
 }
 const BottomPlayers = ({player, hand} : BottomPlayersProp) => {
     const [avatar, setAvatar] = useState("dog")
+    const [pending, setPending] = useState(player.pending);
 
     useEffect(() => {
         async function fetchAvatar() {
@@ -30,6 +32,7 @@ const BottomPlayers = ({player, hand} : BottomPlayersProp) => {
             }
         }
         fetchAvatar();
+        setPending(player.pending);
     }, [player]);
 
 
@@ -39,11 +42,12 @@ const BottomPlayers = ({player, hand} : BottomPlayersProp) => {
         <div className={"bg-foreground rounded-lg p-2 border shadow flex flex-row items-center"}>
             <div className={"flex justify-center grid-flow-col grid-rows-1"}/>
             <div className="flex justify-center grid-flow-col grid-rows-1">
-                <div id="${slotId}-cards"><CardHand initcards={hand}/></div>
-                <div id="${slotId}-draw" className="flex justify-center"></div>
+                <div id={"${slotId}-cards"}><CardHand initcards={hand}/></div>
             </div>
             <div id="${slotId}-username" className="text-center font-bold mb-2 flex flex-col">
-                <img src={`/images/avatars/${avatar}.png`} alt={`${player.userName}'s avatar`} className={"h-14 w-14"}/>{player.userName}
+                <img src={`/images/avatars/${avatar}.png`} alt={`${player.userName}'s avatar`} className={"h-14 w-14"}/>
+                {player.userName}
+                {player.pending && <div id={"${slotId}-draw"} className="flex justify-center"><Card card={pending}/></div>}
             </div>
 
         </div>

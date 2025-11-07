50fdd25004cf8d1a40f70730ade669a819346e38
Wyson Kong <wyson.kong.mil@army.miil>
2025-11-07 15:53:28 -0600

    refactored to allow users to choose their cardtheme

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -6,7 +6,7 @@ import {
     CardContent,
     CardHeader,
     CardTitle
-}from "@/components/ui/card"
+} from "@/components/ui/card"
 import {
     Table,
     TableBody,
@@ -17,53 +17,52 @@ import {
     TableRow,
 } from "@/components/ui/table"
 import Avatar from "@/components/player/avatar.tsx";
-import { useUser} from "@/components/providers/UserProvider.tsx";
+import {useUser} from "@/components/providers/UserProvider.tsx";
 import CardThemes from "@/components/player/CardThemes.tsx";
 
 export default function Profile() {
     const {user} = useUser();
 
 
-
-
     return (
 
-            <div className={"h-screen flex items-center"}>
-                {user && <Card className="overflow-hidden rounded-md border bg-foreground text-background w-1/3 mx-auto margin flex items-center">
-                    <CardHeader className={"flex justify-center"}><CardTitle>{user.username}</CardTitle>
-                    </CardHeader>
-                    <img src={`/images/avatars/${user.avatar}.png`} height={200} width={200}/>
-                    <CardContent>
-                        <Table className={"bg-foreground text-background"}>
-                            <TableCaption>Player Record</TableCaption>
-                            <TableHeader>
-                                <TableRow>
-                                    <TableHead>Wins</TableHead>
-                                    <TableHead>Losses</TableHead>
-                                    <TableHead>Win Percentage</TableHead>
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                                <TableRow>
-                                    <TableCell>{user.wins}</TableCell>
-                                    <TableCell>{user.loses}</TableCell>
-                                    <TableCell>
-                                        {user.wins + user.loses === 0
-                                            ? 0
-                                            : ((user.wins / (user.wins + user.loses)) * 100).toFixed(2) + "%"}
-                                    </TableCell>
-                                </TableRow>
-                            </TableBody>
-                        </Table>
-                    </CardContent>
-                    <CardAction>
+        <div className={"h-screen flex items-center"}>
+            {user && <Card
+                className="overflow-hidden rounded-md border bg-foreground text-background w-1/3 mx-auto margin flex items-center">
+                <CardHeader className={"flex justify-center"}><CardTitle>{user.username}</CardTitle>
+                </CardHeader>
+                <img src={`/images/avatars/${user.avatar}.png`} height={200} width={200}/>
+                <CardContent>
+                    <Table className={"bg-foreground text-background"}>
+                        <TableCaption>Player Record</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Wins</TableHead>
+                                <TableHead>Losses</TableHead>
+                                <TableHead>Win Percentage</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>{user.wins}</TableCell>
+                                <TableCell>{user.loses}</TableCell>
+                                <TableCell>
+                                    {user.wins + user.loses === 0
+                                        ? 0
+                                        : ((user.wins / (user.wins + user.loses)) * 100).toFixed(2) + "%"}
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </CardContent>
+                <CardAction>
+                    <div className={"flex flex-col justify-between"}>
                         <Avatar/>
                         <CardThemes/>
-                    </CardAction>
-                </Card>}
-            </div>
-
-
+                    </div>
+                </CardAction>
+            </Card>}
+        </div>
 
 
     )
